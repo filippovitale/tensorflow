fe9b5008ff63a70e4092cdc7968b1327a9470f77
Filippo Vitale <fvitale@gmail.com>
2016-05-10 11:52:25 -0400

    Assignment 1 / Problem 1

---
+++ tensorflow/examples/udacity/start.sh
@@ -0,0 +1,2 @@
+#docker run -p 8888:8888 -v /Users/filippovitale/Documents/Projects/tensorflow/tensorflow/examples/udacity:/notebooks -it --rm $USER/assignments
+docker run -p 8888:8888 -v /Users/filippovitale/Documents/Projects/tensorflow/tensorflow/examples/udacity:/notebooks -it --rm b.gcr.io/tensorflow-udacity/assignments:0.5.0
